fecd51db12bbe5187095c7a5ca3700928e85ee11
AshleyCheny <AshleyCheny@gmail.com>
2017-10-07 22:56:22 -0700

    Added the route authentication

--- app.js
+++ app.js
@@ -13,14 +13,29 @@ require('./app_api/models/db');
 // Bring in the Passport config after model is defined
 require('./app_api/config/passport');
 
+// Bring in the routes for the API (delete the default routes)
+var routesApi = require('./app_api/routes/index');
+
+var app = express();
+
 // Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
 // Use the API routes when path starts with /api
 app.use('/api', routesApi);
 
-var app = express();
 app.get('/', (req, res) => {
   res.send('HEY!')
+});
+
+
+
+// error handlers
+// Catch unauthorised errors
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({"message" : err.name + ": " + err.message});
+  }
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
---
+++ app_api/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('express-jwt');
+
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+var ctrlProfile = require('../controllers/profile');
+var ctrlAuth = require('../controllers/authentication');
+
+// apply the route authentication
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+module.exports = router;
