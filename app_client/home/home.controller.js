79f62dd65db151a99d9bf3a070304d26edcb687c
AshleyCheny <AshleyCheny@gmail.com>
2017-10-08 09:52:52 -0700

    Added the angular app

---
+++ app_client/home/home.controller.js
@@ -0,0 +1,11 @@
+(function() {
+
+  angular
+    .module('educatinforall-angular-app')
+    .controller('homeCtrl', homeCtrl);
+
+    function homeCtrl () {
+      console.log('Home controller is running');
+    }
+
+})();
