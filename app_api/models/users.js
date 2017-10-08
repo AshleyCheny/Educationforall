6889c25ae267c6063d2451080437cec961ae1b2c
AshleyCheny <AshleyCheny@gmail.com>
2017-10-07 21:19:06 -0700

    Created the mongoose schema for the mongodb Used Crypto module to create a setPassword and a validPassword method for the schema

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,4 +1,3 @@
-// set the user schema using mongoose
 /*
 ** email is set to be unique as it will be used for the login credentails
 ** hash and salt will be used instead of saving a password
@@ -10,6 +9,10 @@
 ** If the output of this matches the hash, then the password must have been correct.
 */
 
+// use Node.js module crypto to help setting and checking the password
+var crypto = require('crypto');
+
+// set the user schema using mongoose
 var userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -23,3 +26,21 @@ var userSchema = new mongoose.Schema({
   hash: String,
   salt: String
 });
+
+// create a new method "setPassword" for the userSchema that takes one parameter password
+// "setPassword" method will be used when creating a user(register),
+// instead of saving the password to a "password" path
+// we will be able to pass it to the setPassword function to set the salt and hash paths in the user document.
+userSchema.methods.setPassword = function(password){
+  // use randomBytes() to create the random salt
+  this.salt = crypto.randomBytes(16).toString('hex');
+  // use pbkdf2Sync() to create the hash
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+// create another method called validPassword() to check the password
+// this method will be used when users try to login
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
